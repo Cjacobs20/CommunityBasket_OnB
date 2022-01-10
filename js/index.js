81b55bff357ff336e13319e9aa73d3655f754829
Cjacobs20 <83181420+Cjacobs20@users.noreply.github.com>
2022-01-09 17:44:49 -0700

    ywstjetyj

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-console.log("this that js shit dog");
+var modal = document.getElementById('modal')
+var mybutton1 = document.getElementById('button1')
+var mybutton2 = document.getElementById('button2')
+var mybutton3 = document.getElementById('button3')
+var closebutton1 = document.getElementById('closebutton1')
+
+// opens modal on button click
+mybutton1.onclick = function () {
+  modal.style.display = "block";
+}
+
+mybutton2.onclick = function () {
+  modal.style.display = "block";
+}
+
+mybutton3.onclick = function () {
+  modal.style.display = "block";
+}
+
+// closes modal on 'X' click
+closebutton1.onclick = function () {
+  modal.style.display = "none";
+}
+
+// closes modal on outside click
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
