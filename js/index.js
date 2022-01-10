484f7669138ad65f60c1bef824d48254822a288f
Cjacobs20 <83181420+Cjacobs20@users.noreply.github.com>
2022-01-09 20:06:29 -0700

    pppppp

--- js/index.js
+++ js/index.js
@@ -4,23 +4,34 @@ var mybutton2 = document.getElementById('button2')
 var mybutton3 = document.getElementById('button3')
 var closebutton1 = document.getElementById('closebutton1')
 
+mybutton1.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
 // opens modal on button click
-mybutton1.onclick = function () {
+/* mybutton1.onclick = function () {
   modal.style.display = "block";
-}
+} */
+mybutton1.addEventListener("click", function () {
+  modal.style.display = "block";
+});
 
-mybutton2.onclick = function () {
+mybutton2.addEventListener("click", function () {
   modal.style.display = "block";
-}
+});
 
-mybutton3.onclick = function () {
+mybutton3.addEventListener("click", function () {
   modal.style.display = "block";
-}
+});
 
 // closes modal on 'X' click
-closebutton1.onclick = function () {
+/* closebutton1.onclick = function () {
   modal.style.display = "none";
-}
+} */
+
+closebutton1.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
 // closes modal on outside click
 window.onclick = function(event) {
@@ -28,3 +39,9 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+mybutton1.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
